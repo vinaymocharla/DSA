26ab7252726a08c1d7c1e1470f07afe933489d61
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-02-20 09:39:38 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (24.78%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        rows,cols,squares = defaultdict(set),defaultdict(set),defaultdict(set)
+
+
+        for r in range(9):
+            for c in range(9):
+                ch =board[r][c]
+                if ch=='.':
+                    continue
+                
+                if (ch in rows[r] or ch in cols[c] or ch in squares[(r//3,c//3)]):
+                    return False
+                
+                rows[r].add(ch)
+                cols[c].add(ch)
+                squares[(r//3,c//3)].add(ch)
+
+        return True
+
+
+        
