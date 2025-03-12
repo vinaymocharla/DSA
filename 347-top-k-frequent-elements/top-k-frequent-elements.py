2af01929966cf2e3f5d7df0008c1ff0bf4cd391d
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-03-12 07:59:11 -0500

    Time: 7 ms (51.85%) | Memory: 24.3 MB (13.15%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,29 +1,30 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
 
-        freq = [[] for i in range(len(nums)+1)]
+        freq= [[] for i in range(len(nums)+1)]
 
         map={}
 
         res=[]
 
-        for i in nums:
-            map[i]=map.get(i,0)+1
 
-        
+        for i in nums:
+
+            map[i] = map.get(i,0)+1
+
+
         for key,value in map.items():
 
             freq[value].append(key)
 
-        for i in range(len(freq)-1,-1,-1):
+        
+
+        for i in range(len(freq)-1,0,-1):
 
             for j in freq[i]:
 
                 res.append(j)
 
                 if len(res)==k:
+
                     return res
-
-        
-
-        
