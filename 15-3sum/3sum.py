efa47af36661cc086830c424104f90b69271402b
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-03-13 08:02:33 -0500

    Time: 1559 ms (9.16%) | Memory: 20.3 MB (89.80%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -2,13 +2,13 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
 
 
-        out = set()
+
+        res = set()
 
         nums.sort()
 
-        for i in range(len(nums)-2):
 
-            if i>0 and nums[i]==nums[i-1]:continue
+        for i in range(len(nums)-2):
 
             j=i+1
             k=len(nums)-1
@@ -17,21 +17,17 @@ class Solution:
 
                 sum = nums[i]+nums[j]+nums[k]
 
-                if sum>0:
-                    k-=1
-                elif sum<0:
+                if sum == 0:
+
+                    res.add((nums[i],nums[j],nums[k]))
+
                     j+=1
+                    k-=1
+
+                elif sum>0:
+                    k-=1
                 else:
-
-                    out.add((nums[i],nums[j],nums[k]))
-
                     j+=1
-                    k-=1
 
-                    while j<k and nums[j]==nums[j-1]:j+=1
-                    while j<k and nums[k]==nums[k+1]:k-=1
-
-        return list(out)
-
-
+        return list(res)
         
