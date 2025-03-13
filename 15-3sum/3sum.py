a3633b5b13d063c6187479249d8c470d23a293b4
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-03-13 08:04:28 -0500

    Time: 859 ms (23.31%) | Memory: 20.3 MB (91.22%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -10,6 +10,9 @@ class Solution:
 
         for i in range(len(nums)-2):
 
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+
             j=i+1
             k=len(nums)-1
 
@@ -26,8 +29,11 @@ class Solution:
 
                 elif sum>0:
                     k-=1
+                    while k>j and nums[k+1]==nums[k]: k-=1
                 else:
                     j+=1
+
+                    while j<k and nums[j-1]==nums[j]: j+=1
 
         return list(res)
         
