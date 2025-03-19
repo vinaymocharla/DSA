1544b1139305c35f29721b36257b5be880ffa502
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-03-19 08:48:09 -0500

    Time: 390 ms (56.71%) | Memory: 17.8 MB (84.08%) - LeetSync

--- 18-4sum/4sum.py
+++ 18-4sum/4sum.py
@@ -27,11 +27,11 @@ class Solution:
 
                         l-=1
 
-                        while(l>k and nums[l]==nums[l+1]):l-=1
+                        
                     elif sum< target:
 
                         k+=1
-                        while(k<l and nums[k]==nums[k-1]):k+=1
+                        
                     else:
 
                         out.add((nums[i],nums[j],nums[k],nums[l]))
