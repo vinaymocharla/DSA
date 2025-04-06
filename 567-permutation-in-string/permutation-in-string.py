61f1ad798c2e1cb2bfa110cc5d09fb6990fbee99
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-04-06 15:55:00 -0500

    Time: 26 ms (48.55%) | Memory: 17.9 MB (40.59%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,33 +1,29 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
-
         map1={}
-
         map2={}
 
         for i in s1:
 
             map1[i] = map1.get(i,0)+1
+        
 
-        l,r,maxfreq=0,0,0
+        l=0
+        r=0
 
         while(r!=len(s2)):
 
-
             map2[s2[r]] = map2.get(s2[r],0)+1
 
-           
 
-            while ((r-l+1) > len(s1)):
+            while((r-l+1)>len(s1)):
 
-                map2[s2[l]]=map2.get(s2[l])-1
+                map2[s2[l]] = map2.get(s2[l])-1
 
                 if map2.get(s2[l])==0:
                     map2.pop(s2[l])
-
                 l+=1
-            
 
             if map1==map2:
                 return True
@@ -35,4 +31,6 @@ class Solution:
             r+=1
 
         return False
+
+
         
