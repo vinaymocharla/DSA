332e27cdc6e2796ec3583a6ba3fca324370afb39
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-04-06 15:50:39 -0500

    Time: 129 ms (55.43%) | Memory: 17.9 MB (86.95%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,28 +1,27 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
 
+        freqmap = {}
+
+        maxval,ans = 0,0
+
         l=0
-
         r=0
-
-        map = {}
-
-        ans=0
-
-        maxfreq=0
 
         while(r!=len(s)):
 
-            map[s[r]] = map.get(s[r],0)+1
+            freqmap[s[r]] = freqmap.get(s[r],0)+1
 
-            maxfreq = max(maxfreq,map.get(s[r]))
+            maxval = max(maxval,freqmap.get(s[r]))
 
-            while((r-l+1)-maxfreq > k):
 
-                map[s[l]]= map.get(s[l])-1
+            while((r-l+1)-maxval >k):
+
+                freqmap[s[l]] = freqmap.get(s[l])-1
+
                 l+=1
 
-            ans= max(ans,(r-l+1))
+            ans = max(ans,(r-l+1))
 
             r+=1
 
