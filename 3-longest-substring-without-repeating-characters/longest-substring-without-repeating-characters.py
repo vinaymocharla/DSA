1b74bbb2e163117871e92b2239878645478b08aa
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-04-19 16:51:56 -0500

    Time: 15 ms (81.81%) | Memory: 18 MB (34.39%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,20 +3,21 @@ class Solution:
 
         myset= set()
 
-        l=0
-        r=0
+        maxi=0
 
-        ans=0
+        l,r=0,0
 
         while(r!=len(s)):
 
             while s[r] in myset:
-
                 myset.remove(s[l])
                 l+=1
 
             myset.add(s[r])
-            ans=max(ans,(r-l+1))
+
+            maxi=max(maxi, r-l+1)
+
             r+=1
-        return ans
+
+        return maxi
         
