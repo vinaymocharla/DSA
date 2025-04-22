5b9470b02aec21e2bca7d74951eba9879ae0e35e
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-04-21 22:25:19 -0500

    Time: 79 ms (56.98%) | Memory: 18.1 MB (93.34%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -5,33 +5,35 @@ class Solution:
         map2={}
 
         for i in t:
-            map1[i]=map1.get(i,0)+1
 
-        l,r=0,0
+            map1[i] = map1.get(i,0)+1
 
         formed=0
 
-        window=[-1,0,0]
+        output=[-1,0,0]
+
+        l,r=0,0
 
         while(r!=len(s)):
 
             map2[s[r]] = map2.get(s[r],0)+1
 
             if s[r] in t and map1[s[r]]==map2[s[r]]:
-
                 formed+=1
 
-            while(formed==len(map1)):
+            while formed == len(map1):
 
                 windowsize = r-l+1
 
-                if window[0]==-1 or windowsize<window[0]:
+                if output[0]== -1 or output[0]>windowsize:
 
-                    window[0]=windowsize
-                    window[1]=l
-                    window[2]=r
+                    output[0]=windowsize
+
+                    output[1]=l
+                    output[2]=r
 
                 if s[l] in t and map1[s[l]]==map2[s[l]]:
+
                     formed-=1
 
                 map2[s[l]]-=1
@@ -40,7 +42,15 @@ class Solution:
                     del map2[s[l]]
 
                 l+=1
+
+                
+
             r+=1
 
-        return "" if window[0]==-1 else s[window[1]:window[2]+1]
+        return "" if output[0]==-1 else s[output[1]:output[2]+1]
+
+
+
+
+
         
