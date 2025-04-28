6b098b0b9c4e109242cc971d4bd2abb0a1ff9f38
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-04-28 09:49:24 -0500

    Time: 13 ms (49.90%) | Memory: 17.8 MB (64.20%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,14 +2,18 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
         map1={}
-        map2={}
+
 
         for i in s:
             map1[i] = map1.get(i,0)+1
         
         for j in t:
-            map2[j] = map2.get(j,0)+1
+            if j in map1:
+                map1[j]-=1
+                if map1[j]==0:
+                    del map1[j]
+            else:
+                return False
+        return len(map1) ==0
 
-        return map1==map2
-        
         
