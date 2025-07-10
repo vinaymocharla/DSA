a5368cb136f1360445a7e238b8863ef48b8325be
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-07-10 15:57:07 -0500

    Time: 0 ms (100.00%) | Memory: 42.5 MB (17.28%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,49 +1,62 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        Map<Character, String > map = new HashMap();
-        List<String>  result = new ArrayList();
+        List<String> result = new ArrayList();
 
-        if (digits.length()==0 || digits==null){
+        if(digits==null || digits.length()==0){
+
             return result;
         }
+
+
         
+        Map<Character, String> map = new HashMap<>();
 
         map.put('2',"abc");
         map.put('3',"def");
         map.put('4',"ghi");
         map.put('5',"jkl");
         map.put('6',"mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
 
-        backtrack(0, digits, map,new StringBuilder(), result);
+        backtrack(0, digits, map, new StringBuilder(), result);
+
         return result;
-
-        
-
 
 
         
     }
 
-    private void backtrack(int index, String digits, Map<Character, String> map, StringBuilder current, List<String> result ){
+    private void backtrack(int index,String digits, Map<Character, String> map,StringBuilder words,List<String> result ){
+
+
+
 
 
         if(index==digits.length()){
-            result.add(current.toString());
+
+            result.add(words.toString());
             return;
+
+
+
         }
 
-        String letters  = map.get(digits.charAt(index));
+        String letters = map.get(digits.charAt(index));
 
         for(char ch : letters.toCharArray()){
 
-            current.append(ch);
+            words.append(ch);
 
-            backtrack(index+1, digits,map,current,result);
+            backtrack(index+1, digits, map, words, result);
 
-            current.deleteCharAt(current.length()-1);
+            words.deleteCharAt(words.length()-1);
+
+
+
         }
+
+
     }
 }
