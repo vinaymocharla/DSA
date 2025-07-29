77aa34560210c8c1b7867f1eb964f90ecdf01b58
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-07-29 16:39:24 -0500

    Time: 4 ms (35.07%) | Memory: 45.5 MB (8.18%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,29 +1,32 @@
 class Solution {
+
     Set<Integer> cols = new HashSet();
     Set<Integer> posDiag = new HashSet();
     Set<Integer> negDiag = new HashSet();
     List<List<String>> result = new ArrayList();
     public List<List<String>> solveNQueens(int n) {
 
-        char[][] board = new char [n][n];
-
+        char[][] board = new char[n][n];
 
         for(int i=0;i<n;i++){
-            for (int j=0;j<n;j++){
+            for(int j=0;j<n;j++){
 
-                board[i][j]='.';
+                board[i][j] = '.';
             }
         }
 
-        backtrack(0,board,n);
+        backtrack(0, board,n);
+
         return result;
+
         
     }
 
-    public void backtrack(int r, char[][] board,int n  ){
+    public void backtrack(int r, char[][] board, int n){
 
-        List<String> copy = new ArrayList();
         if(r==n){
+
+            List<String> copy = new ArrayList();
 
             for(char[] row:board){
 
@@ -31,13 +34,11 @@ class Solution {
             }
 
             result.add(copy);
-            return;
-
         }
 
         for(int c=0;c<n;c++){
 
-            if(cols.contains(c) || posDiag.contains(r+c) || negDiag.contains(r-c)){
+            if(cols.contains(c)|| posDiag.contains(r+c) || negDiag.contains(r-c)){
 
                 continue;
             }
@@ -46,17 +47,16 @@ class Solution {
             posDiag.add(r+c);
             negDiag.add(r-c);
 
-            board[r][c] = 'Q';
+            board[r][c]='Q';
 
-            backtrack(r+1,board,n );
+            backtrack(r+1,board,n);
 
             cols.remove(c);
             posDiag.remove(r+c);
             negDiag.remove(r-c);
-            board[r][c] = '.';
+
+            board[r][c]='.';
 
         }
-
-
     }
 }
