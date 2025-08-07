99d52f6f3c72001f6af215396b40e4dbaaacae48
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-08-07 18:48:51 -0500

    Time: 0 ms (100.00%) | Memory: 43 MB (17.90%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -3,27 +3,22 @@ class Solution {
 
         List<List<Integer>> result = new ArrayList();
 
-        List<Integer> list = new ArrayList();
-
-        backtrack(0,nums,list,result);
+        backtrack(0,nums, new ArrayList(),result);
 
         return result;
-
-
         
     }
 
+    public void backtrack(int start, int[] nums,List<Integer> list,List<List<Integer>>  result ){
 
-    public void backtrack(int start, int[] nums,List<Integer> list,List<List<Integer>> result  ){
 
         result.add(new ArrayList(list));
+
 
         for(int i=start;i<nums.length;i++){
 
             list.add(nums[i]);
-
-            backtrack(i+1, nums,list,result);
-
+            backtrack(i+1,nums,list,result);
             list.remove(list.size()-1);
         }
     }
