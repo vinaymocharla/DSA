b4a8d601c36e2360cf79a4b48fef4811ef45d281
Vinay Mocharla <vinaymocharla8@gmail.com>
2025-11-25 00:59:34 -0600

    Time: 42 ms (45.09%) | Memory: 82.1 MB (23.93%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.ts
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.ts
@@ -1,33 +1,27 @@
 function longestConsecutive(nums: number[]): number {
-    
 
-    let set = new Set<number>();
+    const set = new Set<number>();
 
     for(let i of nums){
 
         set.add(i)
     }
-
-    let longest=0;
-    
+    let longest =0;
 
     for(let i of set){
-        
-        
 
         if(!set.has(i-1)){
 
             let length=1
-        
 
-        while(set.has(i+length)){
+            while(set.has(i+length)){
 
-            length++;
-        }
+                length++;
+            }
 
-        longest = Math.max(longest,length)
+            longest = Math.max(longest,length)
         }
     }
-
-    return longest;
+    
+    return longest
 };
